45f54ff6a8eb63364ea016e3fee139216a507e8b
Abdullah Faruk Gönüllü <contact@afaruk.dev>
2021-08-14 00:25:00 +0300

    build: initial express server

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import config from './config/config';
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.listen(config.PORT, () => {
+  console.log(config.PORT);
+  console.log(`Server running on port ${config.PORT}`);
+});
